b3dc5a1e1c1dde4d443923507c57708ead994b19
Tomtao626 <751825253@qq.com>
2021-01-03 17:07:47 +0800

    自定义validator

--- chapter06/form_validator_demo/front/forms.py
+++ chapter06/form_validator_demo/front/forms.py
@@ -6,9 +6,55 @@
 # @Project : form_validator_demo
 
 from django import forms
+from django.core import validators
+from django.views.generic import View
+
+from front.models import User
 
 
-class MyForm(forms.Form):
+class BaseForm(forms.Form):
+    # 提取错误信息
+    def get_errors(self):
+        errors = self.errors.get_json_data()
+        new_errors = dict()
+        for key, error_dicts in errors.items():
+            messages = list()
+            for message in error_dicts:
+                messages.append(message['message'])
+            new_errors[key] = messages
+        return new_errors
+
+
+class MyForm(BaseForm):
     # email = forms.EmailField(error_messages={'invalid': '请输入正确的邮箱'})
     # price = forms.FloatField(error_messages={'invalid': '请输入浮点类型的数据'})
-    person_website = forms.URLField(error_messages={'invalid': '请输入合法的网址', 'required':'请输入一个网址'})
+    # person_website = forms.URLField(error_messages={'invalid': '请输入合法的网址', 'required':'请输入一个网址'})
+    # email = forms.CharField(validators=[validators.EmailValidator(message='请输入正确的邮箱地址')])
+    telephone = forms.CharField(validators=[validators.RegexValidator(r'1[345678]\d(9)',
+                                                                      message='请输入一个合法的手机号')])
+
+
+class RegisterForm(BaseForm):
+    username = forms.CharField(max_length=100)
+    telephone = forms.CharField(validators=[validators.RegexValidator(r'1[345678]\d(9)',
+                                                                      message='请输入一个合法的手机号')])
+    pwd1 = forms.CharField(max_length=20, min_length=6)
+    pwd2 = forms.CharField(max_length=20, min_length=6)
+
+    # 自定义验证器
+    def clean_telephone(self):
+        telephone = self.cleaned_data.get('telephone')
+        exists = User.objects.filter(telephone=telephone).exists()
+        if exists:
+            raise forms.ValidationError(message=f'{telephone}已被注册')
+        return telephone
+
+
+    # 验证多个字段 就需要重写clean方法
+    def clean(self):
+        cleaned_data = super().clean()
+        pwd1 = cleaned_data.get('pwd1')
+        pwd2 = cleaned_data.get('pwd2')
+        if pwd1 != pwd2:
+            raise forms.ValidationError(message='两次密码输入不一致')
+        return cleaned_data
--- chapter06/form_validator_demo/front/views.py
+++ chapter06/form_validator_demo/front/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
-from .forms import MyForm
+from .forms import MyForm, RegisterForm
 # Create your views here.
+from .models import User
 
 
 class IndexView(View):
@@ -15,3 +16,19 @@ class IndexView(View):
             print(form.errors.get_json_data())
             return HttpResponse("fail")
         return HttpResponse("success")
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'register.html')
+
+    def post(self, request):
+        form = RegisterForm(request.POST)
+        # 判断数据是否合法
+        if not form.is_valid():
+            print(form.get_errors())
+            return HttpResponse("注册失败")
+        username = form.cleaned_data.get('username')
+        telephone = form.cleaned_data.get('telephone')
+        User.objects.create(username=username, telephone=telephone)
+        return HttpResponse("注册成功")
